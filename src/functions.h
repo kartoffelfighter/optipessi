f65480e2706f1a0a3bc1eb41d841264cddd85d5e
marc <marc@marcfischer.org>
2018-06-23 20:33:18 +0200

    added standby function, added serial function description

--- src/functions.h
+++ src/functions.h
@@ -76,4 +76,61 @@ void lightMeUp()
     analogWrite(dac1, 255);
     analogWrite(dac2, 255);
     analogWrite(dac3, 255);
+
+    digitalWrite(ledOpti, 0x1);
+    digitalWrite(ledPessi, 0x1);
+}
+
+void standby() {
+        uint32_t beginning = millis();
+
+        digitalWrite(mux1, 0); // voll
+        digitalWrite(mux2, 0); // leer resetten
+        digitalWrite(mux3, 1); // oben
+        digitalWrite(mux4, 0); // disable blue mux
+        fadeUp(1);
+        digitalWrite(ledOpti, 0x1);
+        digitalWrite(ledPessi, 0x1);
+
+        while(millis() - beginning <= 10*1000 && !runOptimist && !runPessimist){
+            digitalWrite(mux3, 0);
+            digitalWrite(ledOpti, 0);
+            digitalWrite(ledPessi, 0);
+            delay(delayWhileLightshow);
+            digitalWrite(mux3, 1);
+            digitalWrite(ledOpti, 1);
+            digitalWrite(ledPessi, 1);
+            delay(delayWhileLightshow);
+        }
+        resetLight();
+}
+
+void serialOpen() {
+  Serial.println("hello - optipessi v1 here!");
+  Serial.print("runtime: ");
+  Serial.print(millis()/1000/60);
+  Serial.println("m");
+  Serial.println("--------------------");
+  Serial.println("My current Settings:");
+  Serial.print("stayActive (time to stay active after a button was pressed): ");
+  Serial.print(stayActive / 1000);
+  Serial.println("s");
+  Serial.print("lightShow (time to start the light show after no button was pressed): ");
+  Serial.print(lightShow);
+  Serial.println("m");
+  Serial.println("--------------------");
+  Serial.println("Possible Commands:");
+  Serial.println("|send|action|");
+  Serial.println("| o  |turns of the lights|");
+  Serial.println("| d  |turn on all possible lights|");
+  Serial.println("| p  |run pessimist|");
+  Serial.println("| q  |run optimist");
+  Serial.println("| e  |freak out");
+  Serial.println("| s  |reprint this message");
+  Serial.println("| a  |adjust stayActive to n seconds");
+  Serial.println("| l  |adjust lightShow to n minutes");
+  Serial.println("| g  |adjust lightShow flash rate in ms");
+  Serial.println("--------------------");
+  Serial.println("ready");
+
 }
